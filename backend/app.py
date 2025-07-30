72c8168af2dba2ce21b003a678a8888bc5a654c5
louis nguyen <louis.nguyen550@gmail.com>
2025-07-30 12:39:38 -0400

    setup flask as backend

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,12 @@
-from flask import flask
+from flask import Flask
 
+app = Flask(__name__)
+
+# members API route
+@app.route("/members")
+
+def members():
+    return {"members": ["Members1", "Members2", "Members3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
