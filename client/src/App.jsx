d8814801b68ea92d1414adbab6275261e31c308c
Louis Nguyen <louis.nguyen550@gmail.com>
2025-07-31 11:30:28 -0400

    removed junk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
   const [array, setArray] = useState([]);
 
   const fetchAPI = async () => {
@@ -20,31 +17,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        {array.map((user, index) => (
-          <div key={index}>
-            <span>{user}</span>
-            <br></br>
-          </div>
-        ))}
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {array.map((user, index) => (
+        <div key={index}>
+          <span>{user}</span>
+          <br></br>
+        </div>
+      ))}
     </>
   )
 }
