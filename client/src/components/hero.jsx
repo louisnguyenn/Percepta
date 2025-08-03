457a7a4ea62921741afc8b5be836669b214bf39b
louis nguyen <louis.nguyen550@gmail.com>
2025-08-03 19:40:34 -0400

    dashboard pages created

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
   return (
-    <>
-      <h1 className="">Percepta</h1>
-    </>
+    <div className="bg-black min-h-screen max-w-8xl mx-auto">
+      <h1 className="text-center text-6xl font-bold text-white">Percepta</h1>
+    </div>
   )
 }
