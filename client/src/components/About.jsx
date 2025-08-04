a2e23a549704b0854c2d14397aba048c38ea98f3
louis nguyen <louis.nguyen550@gmail.com>
2025-08-03 21:04:55 -0400

    adding about section

---
+++ client/src/components/About.jsx
@@ -0,0 +1,9 @@
+export const About = () => {
+  return (
+    <section className="bg-white/70 p-10">
+      <div className="max-w-6xl mx-auto flex flex-col justify-center items-center z-10">
+          <h2 className="text-4xl text-white">Who we are</h2>
+      </div>
+    </section>
+  )
+}
