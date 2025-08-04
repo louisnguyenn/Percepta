d3f35b0d1a3ff70cf2d876c2c558c307267e2d92
louis nguyen <louis.nguyen550@gmail.com>
2025-08-03 21:14:52 -0400

    started about me boilerplate

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,8 +1,15 @@
 export const About = () => {
   return (
-    <section className="bg-white/70 p-10">
+    <section className="bg-white py-20 min-h-screen">
       <div className="max-w-6xl mx-auto flex flex-col justify-center items-center z-10">
-          <h2 className="text-4xl text-white">Who we are</h2>
+        <h2 className="text-5xl text-[#FF8C42] font-semibold">Who we are</h2>
+        <p className="max-w-5xl p-6 text-center text-xl leading-8">
+          Percepta is a cutting-edge human detection system that leverages advanced computer vision and AI technology to provide real-time monitoring and security solutions. Our platform intelligently identifies and tracks human presence in recorded environments, delivering accurate detection capabilities for enhanced safety and security monitoring.
+        </p>
+
+        <div className="py-20">
+          <h2 className="text-5xl text-[#FF8C42] font-semibold">Our Goal</h2>
+        </div>
       </div>
     </section>
   )
