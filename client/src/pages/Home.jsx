cab97f943b113f190ed30930ab465e822eecc1b8
Louis Nguyen <louis.nguyen550@gmail.com>
2025-08-04 15:38:09 -0400

    created home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Hero } from "../components/sections/Hero";
+import { HowItWorks } from "../components/sections/How-It-Works";
+import { About } from "../components/sections/About";
+import { Footer } from "../components/sections/Footer";
+
+export const Home = () => {
+  return (
+    <>
+      <Hero />
+      <HowItWorks />
+      <About />
+      <Footer />
+    </>
+  );
+};
