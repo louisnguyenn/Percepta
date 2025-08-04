9d1bea275f0c5b36f8d09b9f95b8419c58612e75
louis nguyen <louis.nguyen550@gmail.com>
2025-08-03 20:13:03 -0400

    basic styling for hero page

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -1,11 +1,13 @@
+import { ArrowRight } from "lucide-react"
+
 export const Hero = () => {
   return (
     <div className="min-h-screen max-w-8xl mx-auto flex flex-col justify-center items-center">
       <div className="flex-1 flex items-end justify-center pb-20">
-        <h1 className="text-center text-6xl font-bold text-white">Percepta</h1>
+        <h1 className="text-center text-8xl font-bold text-white">Percepta</h1>
       </div>
       <div className="flex-1 flex items-start justify-center pt-20">
-        <a href="Dashboard.jsx" className="bg-white text-black py-3 px-6 rounded-full font-medium transition-all relative">Get Started</a>
+        <a href="Dashboard.jsx" className="flex items-center gap-2 bg-[#FF8C42] text-black py-3 px-6 rounded-full font-semibold transition-all relative hover:bg-[#E6753A] duration-300">Get Started <ArrowRight size={16}></ArrowRight></a>
       </div>
     </div>
   )
