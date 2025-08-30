c96036be3fd60650b0c18ed2bc2c6dd62d1ae3a3
louis nguyen <louis.nguyen550@gmail.com>
2025-08-29 22:46:59 -0400

    created return route for logo

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ export const Sidebar = () => {
 
   const getLinkClassName = path => {
     const baseClasses =
-      "flex items-center px-4 py-2 text-slate-700 transition-all duration-300 ease-in-out transform hover:scale-[1.04] hover:shadow-md group relative overflow-hidden";
+      "flex items-center px-4 py-2 text-slate-700 transition-all duration-300 ease-in-out transform hover:scale-[1.04] hover:shadow-md group relative overflow-hidden rounded-lg";
     return location.pathname === path
       ? `${baseClasses} bg-[#B0B0B0] font-bold shadow-lg scale-[1.02]`
       : `${baseClasses} hover:bg-[#B0B0B0]`;
@@ -13,7 +13,7 @@ export const Sidebar = () => {
 
   return (
     <nav className="fixed left-0 top-0 py-2 h-screen w-52 bg-[#C6C6C6] shadow-2xl border-r border-gray-300/20">
-      <Link>
+      <Link to="/">
         <a className="text-black transition-all duration-300 text-2xl font-semibold hover:scale-103 hover:font-bold cursor-pointer flex items-center">
           <img src="/logo.png" alt="logo" className="w-18 h-18" />
           Percepta
@@ -47,6 +47,8 @@ export const Sidebar = () => {
           <div className="absolute inset-0 bg-gradient-to-r from-transparent to-white/5 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
         </Link> */}
 
+        {/* go back sidebar option */}
+        
         <Link to="/" className={getLinkClassName("/")}>
           <div className="flex items-center space-x-3">
             <div className="w-2 h-2 rounded-full bg-blue-500 opacity-70 group-hover:opacity-100 transition-opacity duration-300"></div>
